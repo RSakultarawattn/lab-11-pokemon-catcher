ab78835249c7acdcffe16c91e9c6b78a2a66623c
RSakultarawattn <ray.sakultarawattn@gmail.com>
2020-10-13 11:25:21 -0700

    capturing

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import { rawPokeData } from './data.js';
 
 const images = document.querySelectorAll('.pokeIm');
 const radios = document.querySelectorAll('input');
-const button = document.querySelector('button');
+
 
 
 
@@ -17,43 +17,51 @@ export function getRandomPokemon(){
 
 }
 let encounters = 10;
+renderPokemon();
 
-let pokemonOne = getRandomPokemon();
-let pokemonTwo = getRandomPokemon();
-let pokemonThree = getRandomPokemon();
+for (let i = 0; i < radios.length; i++) {
+radios[i].addEventListener('change', (e) => {
+    const captured = e.target.value;
+    console.log(captured)
+    renderPokemon();
+    
+});
+
+}
+
+
+function renderPokemon() {
+let pokemonOne = getRandomPokemon(rawPokeData);
+let pokemonTwo = getRandomPokemon(rawPokeData);
+let pokemonThree = getRandomPokemon(rawPokeData);
 
 while (pokemonOne.id === pokemonTwo.id) {
-    pokemonOne = getRandomPokemon(rawData);
+    pokemonOne = getRandomPokemon(rawPokeData);
 
 }
 while (pokemonTwo.id === pokemonThree.id) {
-    pokemonTwo = getRandomPokemon(rawData);
+    pokemonTwo = getRandomPokemon(rawPokeData);
 
 }
 while (pokemonThree.id === pokemonOne.id) {
-    pokemonThree = getRandomPokemon(rawData);
+    pokemonThree = getRandomPokemon(rawPokeData);
 
 }
 
-radios[0].value = pokemonOne.id;
+radios[0].value = pokemonOne.pokemon;
 images[0].src = pokemonOne.url_image;
 
-radios[1].value = pokemonTwo.id;
+radios[1].value = pokemonTwo.pokemon;
 images[1].src = pokemonTwo.url_image;
 
-radios[2].value = pokemonThree.id;
+radios[2].value = pokemonThree.pokemon;
 images[2].src = pokemonThree.url_image;
-
+}
 
 
 
 
 //set event listeners to update state and DOM
 
-for (let i = 0; i < button.length; i++) {
-    button[i].addEventListener('change', (e) =>
-    {
 
 
-    })
-}

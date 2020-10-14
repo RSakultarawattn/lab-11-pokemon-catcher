562e120ca1e8b5c1ac5db6e4d064c2a94fd7541d
RSakultarawattn <ray.sakultarawattn@gmail.com>
2020-10-13 17:56:58 -0700

    on the way to local storage

---
+++ helper-functions.js
@@ -0,0 +1,17 @@
+export function getFromLocalStorage(key) {
+
+    const item = localStorage.getItem(key);
+
+    return JSON.parse(item);
+
+
+}
+
+export function setInLocalStorage(key, value) {
+
+    const stringyItem = JSON.stringify(value);
+
+    localStorage.setItem(key, stringyItem);
+
+    return value;
+}

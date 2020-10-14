3f51feae185fc7b43ecd5b0fd87311f91339dca6
RSakultarawattn <ray.sakultarawattn@gmail.com>
2020-10-13 17:13:21 -0700

    still more progress

--- utils.js
+++ utils.js
@@ -8,7 +8,7 @@ export function findByName(someArray, name) {
     
     for (let i = 0; i < someArray.length; i++){
       const item = someArray[i];
-        if (item.name === name) {
+        if (item.pokemon === name) {
             
             return item;
         }
